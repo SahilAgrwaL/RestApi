4efe24c3dbd6e1f99a052551436caae84d9f6468
SahilAgarwl <201b226@juetguna.in>
2023-12-16 17:09:34 +0530

    updated Build File

--- main.go
+++ main.go
@@ -55,65 +55,34 @@ func getProfile(q http.ResponseWriter, r *http.Request){
 	json.NewEncoder(q).Encode(profile)
 }
 
+func updateProfile(q http.ResponseWriter, r *http.Request){
+	var idParam string = mux.Vars(r)["id"]
+    id, err := strconv.Atoi(idParam)
+	if err!=nil{
+		q.WriteHeader(400)
+		q.Write([]byte("ID could not be converted to integer"))
+		return
+	}
+	if id>=len(profiles){
+		q.WriteHeader(404)
+		q.Write([]byte("No profile found with specified ID"))
+		return
+	}
+	var updateProfile Profile
+	json.NewDecoder(r.Body).Decode(&updateProfile)
+
+	profiles[id]=updateProfile
+	q.Header().Set("Content-Type","application/json")
+	json.NewEncoder(q).Encode(updateProfile)
+}
+
 func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/profiles", addItem).Methods("POST")
-	router.HandleFunc("/profiles", getAllProfiles).Methods("GEt")
+	router.HandleFunc("/profiles", getAllProfiles).Methods("GET")
     router.HandleFunc("/profiles/{id}", getProfile).Methods("GET")
+	router.HandleFunc("/profiles/{id}", updateProfile).Methods("PUT")
+	
 	http.ListenAndServe(":5000", router)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// package main 
-
-// import (
-// 	"encoding/json"
-// 	"net/http"
-// 	"strconv"
-// 	"github.com/gorilla/mux"
-// )
-
-// var profiles [] profile = [] Profile{}
-
-// type User struct {
-// 	FirstName string `json: "firstName"`
-// 	LastName string `json: "lastname"`
-// 	Email string `json: "email"`
-// }
-// type Profile struct {
-// 	Department string `json:"department"`
-// 	Designation string `json:"designation"`
-// 	Employee User `json: "employee"`
-// }
-
-// func additem(q http.ResponseWriter, r *http.Request){
-//     var newProfile Profile
-// 	json.NewDecoder(r.body).Decode(&newProfile)
-
-// 	w.Header().Set("Content-Type", "application/json")
-
-// 	profiles - append(profiles, newProfile)
-    
-// 	json.Newencoder(q).Encode(profiles)
-// }
-
-// func main (){
-//         router:= mux.NewRouter()
-
-// 		router.HandleFunc("/profiles", additem).Methods("POST")
-
-// 		http.ListenAndServe(":5000", router)
-
-// }
