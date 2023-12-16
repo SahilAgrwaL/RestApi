b849bfdaa066882838258fc1cdabf4cf43376d70
SahilAgarwl <201b226@juetguna.in>
2023-12-16 15:19:05 +0530

    added new lines of code and trying out with postman

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"strconv"
 	"github.com/gorilla/mux"
 )
+
+var profiles []profile = []Profile{}
+
 type User struct {
 	FirstName string `json: "firstName"`
 	LastName string `json: "lastname"`
@@ -17,9 +20,22 @@ type Profile struct {
 	Employee User `json: "employee"`
 }
 
-function main (){
+func additem(q http.ResponseWriter, r "http.Request"){
+    var newProfile Profile
+	json.NewDecoder(r.body).Decode(&newProfile)
+
+	w.Header().Set("Content-Type", application/json)
+
+	profiles - append(profiles, newProfile)
+    
+	json.Newencoder(q).Encode(profiles)
+}
+
+func main (){
         router:= mux.NewRouter()
 
+		router.HandleFuntion("/profiles", additem).Methods("POST")
+
 		http.ListenandServe(":5000", router)
-		
+
 }
